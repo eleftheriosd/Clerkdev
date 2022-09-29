6490619d19c325a7c02446db52f3a45046d266a8
Eleftheriosd <eleftherios.davros@gmail.com>
2022-09-29 18:25:06 +0300

    Adding basic API teset

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+test("Get User Api test", async () => {
+  const req = await fetch(
+    "https://randomuser.me/api/?response=json&page=1&seed=1&results=1&inc=name,email,picture,phone,location,id"
+  );
+  // Test req status
+  expect(req.status).toBe(200);
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const data = await req.json();
+  const results = data.results[0];
+  expect(results).toHaveProperty("email");
 });
